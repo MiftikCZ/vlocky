e0a388decd42d87efc5abd0a6ecd863961a5cfcd
MiftikCZ <memertv17@gmail.com>
2023-04-17 19:51:13 +0200

    Made it more readable

--- recepty.js
+++ recepty.js
@@ -1,37 +1,70 @@
-[
+var recepty = [
     {
         "title": "štrůdl",
         "description": "se skořicovým cukrem",
         "time": "3min",
-        "recept": 
-"40g vloček;špetka soli;1 vrchovatá lžička skořicového cukru;120ml mléka;necháme přes noc v lednici a ráno ohřejeme na 0:40s v mikrovlnce."
+        "recept": [
+            "40g vloček",
+            "špetka soli",
+            "1 vrchovatá lžička skořicového cukru",
+            "120ml mléka",
+            "necháme přes noc v lednici a ráno ohřejeme na 0:40s v mikrovlnce."
+        ]
     },
     {
         "title": "kakao + kokos",
         "description": "granko se strouhaným kokosem",
         "time": "2min",
-        "recept":
-"40g vloček;špetka soli;1 lžička granka;1 lžíce kokosu;3/4 lžičky javorového sirupu/medu;120ml mléka;necháme přes noc v lednici a ráno ohřejeme na 0:40s v mikrovlnce."
+        "recept": [
+            "40g vloček",
+            "špetka soli",
+            "1 lžička granka",
+            "1 lžíce kokosu",
+            "3/4 lžičky javorového sirupu/medu",
+            "120ml mléka",
+            "necháme přes noc v lednici a ráno ohřejeme na 0:40s v mikrovlnce."
+        ]
     },
     {
         "title": "banán + nutella",
         "description": "nejlepší kobinace! :D",
-        "time": "3min", 
-        "recept":
-"40g vloček;špetka soli;1 lžička nutelly/granka;1/2 rozmačkaného banánu;1/2 lžičky javorového sirupu/medu;necháme přes noc v lednici a ráno ohřejeme na 0:40s v mikrovlnce.;*můžeme jěšte na vrch nakrájet zbytek (1/2) banánu"      
+        "time": "3min",
+        "recept": [
+            "40g vloček",
+            "špetka soli",
+            "1 lžička nutelly/granka",
+            "1/2 rozmačkaného banánu",
+            "1/2 lžičky javorového sirupu/medu",
+            "necháme přes noc v lednici a ráno ohřejeme na 0:40s v mikrovlnce.",
+            "*můžeme jěšte na vrch nakrájet zbytek (1/2) banánu"
+        ]
     },
     {
         "title": "domácí müsli",
         "description": "skořicovo banánové vločky orestované na pánvy",
         "time": "8-10min",
-        "recept":
-"40g vloček;špetka soli;1/2 rozmačkaného banánu;2x špetka skořice;1 + 1/3 lžičky javorového sirupu/medu;všechno pečlivě zamýcháme;orestujeme na suché pánvičce 6-8min nebo dokud to nezačně vonět / smrdět"
+        "recept": [
+            "40g vloček",
+            "špetka soli",
+            "1/2 rozmačkaného banánu",
+            "2x špetka skořice",
+            "1 + 1/3 lžičky javorového sirupu/medu",
+            "všechno pečlivě zamýcháme",
+            "orestujeme na suché pánvičce 6-8min nebo dokud to nezačně vonět / smrdět"
+        ]
     },
     {
         "title": "banán + skořice",
         "description": "skořicovo banánové vločky na rychlo",
         "time": "3-4min",
-        "recept":
-"40g vloček;špetka soli;1/2 rozmačkaného banánu;1/2 lžičky skořicového curku;1/2 lžičky javorového sirupu/medu;ohřejeme v mikrovlnce na 1:20s;pak můžeme nakrájet na vrch zbytek banánu (1/2) na kolečka"  
+        "recept": [
+            "40g vloček",
+            "špetka soli",
+            "1/2 rozmačkaného banánu",
+            "1/2 lžičky skořicového curku",
+            "1/2 lžičky javorového sirupu/medu",
+            "ohřejeme v mikrovlnce na 1:20s",
+            "pak můžeme nakrájet na vrch zbytek banánu (1/2) na kolečka"
+        ]
     }
 ]
